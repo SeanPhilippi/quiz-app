43117a7728d198b72cef15c89862a3a95d74ad5c
SeanPhilippi <sean.philippi@gmail.com>
2020-11-14 15:03:47 -0600

    added QuestionCard, started api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
 
+const totalQuestion = 10;
+
 const App = () => {
+    const [loading, setLoading] = useState(false);
+    const [questions, setQuestions] = useState([]);
+    const [number, setNumber] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+    const [score, setScore] = useState(0);
+    const [gameOver, getGameOver] = useState(true);
+
     const startTrivia = async () => {
 
     }
@@ -20,9 +29,20 @@ const App = () => {
             <button className='start' onClick={startTrivia}>
                 Start
             </button>
+            <p className='score'>Score:</p>
+            {/* replace with spinner later */}
             <p>Loading Questions...</p>
-            <QuestionCard />
-            <button className='next' onClick={nextQuestion}></button>
+            <QuestionCard
+                questionNum={number + 1}
+                totalQuestions={totalQuestions}
+                question={questions[number].question}
+                answers={questions[number].answers}
+                userAnswer={userAnswers && userAnswers[number]}
+                callBack={checkAnswer}
+            />
+            <button className='next' onClick={nextQuestion}>
+                Next question
+            </button>
         </div>
     )
 }
---
+++ src/api.tsx
@@ -0,0 +1,10 @@
+// enum is a typescript thing
+export enum Difficulty {
+    EASY: 'easy',
+    MEDIUM: 'medium',
+    HARD: 'hard',
+}
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+}
